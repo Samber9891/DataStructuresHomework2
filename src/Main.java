cac565b5ace3a1674a84d53c6e1d3a25ec6bd199
Samber9891 <Slem0119@gmail.com>
2020-10-14 18:20:00 -0400

    init commit

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+
+public class Main {
+
+    public static void main(String[] args) {
+        // int sumArray[] = {5, 10, 15, 20, 25, 30}; 
+        // int sum = 0 ;    
+    
+
+        // for (int i : sumArray)
+        //     sum += i; 
+        // System.out.println("The sum is " + sum);
+
+//  Question 2
+// the output is 2 
+
+int N = 2; 
+int P = 3; 
+
+    }
+}
+    
---
+++ src/Power.java
@@ -0,0 +1,24 @@
+
+public class Power {
+
+ 
+
+// properties 
+
+    
+    static int power(int N, int P) 
+    { 
+        int pow = 1; 
+        for (int i = 1; i <= P; i++) 
+            pow *= N; 
+        return pow; 
+    }
+
+   
+    } 
+ 
+
+
+   
+
+
